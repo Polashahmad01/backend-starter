6e9fe8b7e6336b85cfa836c0f53db2416b2fd03d
Polashahmad01 <dev.polashahmad@gmail.com>
2023-06-26 18:24:32 +0600

    add basic cookies

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,10 +17,10 @@ const registerUser = asyncHandler(async(req, res, next) => {
     
       res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000
-      })
+      })  
     
       return token
     }
