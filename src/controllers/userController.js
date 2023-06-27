2c014d7d8e5489b83b051c3ec29ced7c8834c9ad
Polashahmad01 <dev.polashahmad@gmail.com>
2023-06-28 05:34:08 +0600

    add login route api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -27,6 +27,26 @@ const registerUser = asyncHandler(async(req, res, next) => {
   }
 })
 
+// @desc    Auth or login user and set token
+// route    POST /api/v1/users/auth
+// @access  Public
+const authUserOrLogin = asyncHandler(async(req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if(user && (await user.matchPassword(password))) {
+      generateJwtToken(res, user._id)
+      res.status(200).json({ success: true, data: { id: user._id, name: user.name, email: user.email, isVerified: user.isVerified }})
+    } else {
+      return res.status(400).json({ success: false, message: "Invalid email or password." })
+    }
+  } catch(error) {
+    return next(new ErrorResponse(error.message || "Internal server error", 500))
+  }
+})
+
 export {
-  registerUser
+  registerUser,
+  authUserOrLogin
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,12 +2,18 @@ import express from "express"
 const router = express.Router()
 
 import {
-  registerUser
+  registerUser,
+  authUserOrLogin
 } from "../controllers/userController.js"
 
 // @desc    Register a new user
 // route    POST /api/v1/users
 // @access  Public
 router.post("/", registerUser)
+
+// @desc    Auth or login user and set token
+// route    POST /api/v1/users/auth
+// @access  Public
+router.post("/auth", authUserOrLogin)
 
 export default router
